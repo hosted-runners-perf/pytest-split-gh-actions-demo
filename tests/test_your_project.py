9a73cdc07db8a3d5ee4ea7192816e8687ed00c3d
Matthew Rosenbaum <8013350+mgrosen@users.noreply.github.com>
2024-06-06 11:30:33 -0700

    Update test_your_project.py

--- tests/test_your_project.py
+++ tests/test_your_project.py
@@ -1,41 +1,41 @@
 from my_project import some_functionality
 
 
-def your_test_1():
+def test_your_1():
     some_functionality.do_something1()
 
 
-def your_test_2():
+def test_your_2():
     some_functionality.do_something2()
 
 
-def your_test_3():
+def test_your_3():
     some_functionality.do_something3()
 
 
-def your_test_4():
+def test_your_4():
     some_functionality.do_something4()
 
 
-def your_test_5():
+def test_your_5():
     some_functionality.do_something5()
 
 
-def your_test_6():
+def test_your_6():
     some_functionality.do_something6()
 
 
-def your_test_7():
+def test_your_7():
     some_functionality.do_something7()
 
 
-def your_test_8():
+def test_your_8():
     some_functionality.do_something8()
 
 
-def your_test_9():
+def test_your_9():
     some_functionality.do_something9()
 
 
-def your_test_10():
+def test_your_10():
     some_functionality.do_something10()
