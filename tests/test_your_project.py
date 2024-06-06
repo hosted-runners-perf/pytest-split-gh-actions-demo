14fb721fcf63cb553cc9c2aa28da74d46b57e375
Matthew Rosenbaum <8013350+mgrosen@users.noreply.github.com>
2024-06-06 11:16:56 -0700

    Create test_your_project.py

---
+++ tests/test_your_project.py
@@ -0,0 +1,41 @@
+from my_project import some_functionality
+
+
+def your_test_1():
+    some_functionality.do_something1()
+
+
+def your_test_2():
+    some_functionality.do_something2()
+
+
+def your_test_3():
+    some_functionality.do_something3()
+
+
+def your_test_4():
+    some_functionality.do_something4()
+
+
+def your_test_5():
+    some_functionality.do_something5()
+
+
+def your_test_6():
+    some_functionality.do_something6()
+
+
+def your_test_7():
+    some_functionality.do_something7()
+
+
+def your_test_8():
+    some_functionality.do_something8()
+
+
+def your_test_9():
+    some_functionality.do_something9()
+
+
+def your_test_10():
+    some_functionality.do_something10()
